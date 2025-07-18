ae830f6251dc83b25527e51d93bf389340ec9142
jeyamuthu <110531862+jeyamuthu@users.noreply.github.com>
2025-07-18 12:28:46 +0530

    Update LoginBrowser.java
    written the comment line

--- assignment/LoginBrowser.java
+++ assignment/LoginBrowser.java
@@ -7,24 +7,32 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class LoginBrowser {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
+
+		// open the guest account
        ChromeOptions opt = new ChromeOptions();
        opt.addArguments("guest");
-       
+
+	// create the object for ChromeDriver
        ChromeDriver driver = new ChromeDriver(opt);
+		
+		// Launch the Browser
        driver.get("https://www.saucedemo.com/v1/");
        
-       
+       // maximize the screen
        driver.manage().window().maximize();
-       
+
+		//send the Username
        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-       
+
+		// send the Password
        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-       
+		
+       //click the login button
        driver.findElement(By.id("login-button")).click();
        
        Thread.sleep(3000);
-       
+
+		// close the window
        driver.quit();
        
       
